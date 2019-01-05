39c9c82fc7fa31deeaeed2be49e814e5e1a62129
theirfanullah <theirfanullah@gmail.com>
2019-01-05 18:08:11 +0500

    dialActivity added.

--- app/src/main/java/com/contactsapp/contactsapp/DialActivity.java
+++ app/src/main/java/com/contactsapp/contactsapp/DialActivity.java
@@ -1,19 +1,106 @@
 package com.contactsapp.contactsapp;
 
+import android.content.Context;
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.AppCompatButton;
+import android.support.v7.widget.AppCompatEditText;
+import android.text.Editable;
+import android.text.InputType;
+import android.text.TextWatcher;
+import android.util.Log;
+import android.view.View;
+import android.widget.Adapter;
+import android.widget.AdapterView;
+import android.widget.GridView;
 
-public class DialActivity extends AppCompatActivity {
+public class DialActivity extends AppCompatActivity implements DialAdapter.buttonsClickListener {
 
+    private AppCompatEditText phoneNumberField;
+    GridView gridView;
+    DialAdapter dialAdapter;
+    private Context context;
+    String[] zeroButton = {"0"};
+    String[] buttons = {"1","2","3","4","5","6","7","8","9","0","Cancel","Dial"};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dial);
+        initObjects();
+        watchChangesInTheNumber();
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP)
+            phoneNumberField.setShowSoftInputOnFocus(false);
+        else
+            phoneNumberField.setTextIsSelectable(true);
+    }
+
+    private void watchChangesInTheNumber() {
+        phoneNumberField.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+                onlyZeroButton();
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                Log.i("contacts App: ",s.toString());
+                    if(s.toString().length() >= 1){
+                        AllButtons();
+                    }else {
+                        onlyZeroButton();
+                    }
+            }
+        });
+    }
+
+    private void initObjects() {
+        context = this;
+        phoneNumberField = findViewById(R.id.phoneNumberToDial);
+//        phoneNumberField.setInputType(InputType.TYPE_NULL);
+//        phoneNumberField.setCursorVisible(true);
+//        phoneNumberField.setFocusable(true);
+//        phoneNumberField.setFocusableInTouchMode(true);
+        gridView = findViewById(R.id.dialingButtonsGridView);
+        String[] buttons = {"0"};
+        dialAdapter = new DialAdapter(this,buttons);
+        dialAdapter.setButtonsClickListener(this);
+        gridView.setAdapter(dialAdapter);
+    }
+
+    private void onlyZeroButton(){
+
+        dialAdapter.notifyAdapter(zeroButton);
+    }
+
+    private void AllButtons(){
+
+        dialAdapter.notifyAdapter(buttons);
     }
 
     @Override
     protected void onPause() {
         super.onPause();
         finish();
+    }
+
+    @Override
+    public void onClick(int position) {
+                String numberToAppend = null;
+
+                if(phoneNumberField.getText().toString().length() < 1 ){
+                    numberToAppend = "0";
+                }else if(phoneNumberField.getText().toString().length() >= 1){
+                    numberToAppend =  buttons[position];
+                }
+
+
+                phoneNumberField.setText(phoneNumberField.getText().toString()+numberToAppend);
     }
 }
---
+++ app/src/main/java/com/contactsapp/contactsapp/DialAdapter.java
@@ -0,0 +1,75 @@
+package com.contactsapp.contactsapp;
+
+import android.content.Context;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.widget.AppCompatEditText;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.Button;
+
+public class DialAdapter extends BaseAdapter {
+    private Context context;
+    private String[] numbers;
+    AppCompatEditText phoneNumberField;
+    buttonsClickListener clickListener;
+
+    public DialAdapter(Context context, String[] numbers) {
+        this.context = context;
+        this.numbers = numbers;
+    }
+
+    @Override
+    public int getCount() {
+        return numbers.length;
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    @Override
+    public View getView(final int position, View convertView, ViewGroup parent) {
+        phoneNumberField = parent.findViewById(R.id.phoneNumberToDial);
+        View view = LayoutInflater.from(context).inflate(R.layout.button_grid_dial_layout,parent,false);
+        Button btn = view.findViewById(R.id.dialBtn);
+        if(numbers[position].equals("Cancel")) {
+            btn.setText(numbers[position]);
+        }else if(numbers[position].equals("Dial")){
+            btn.setText(numbers[position]);
+        }else {
+            btn.setText(numbers[position]);
+            btn.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    clickListener.onClick(position);
+                }
+            });
+        }
+
+
+
+        return view;
+    }
+
+    public void notifyAdapter(String[] numbers) {
+        this.numbers = numbers;
+        notifyDataSetChanged();
+    }
+
+
+    public interface buttonsClickListener{
+        public void onClick(int position);
+    }
+
+    public void setButtonsClickListener(buttonsClickListener clickListener){
+        this.clickListener = clickListener;
+    }
+}
